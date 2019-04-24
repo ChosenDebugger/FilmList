be73380265f4e901b2536fba81910b8ab01818ce
齐旭晨 <1023578360@qq.com>
2019-04-25 02:44:34 +0800

    02:44

--- main.js
+++ main.js
@@ -4,27 +4,17 @@
 //
 'use strict';
 
-var data;
-
-function init(){
-    const url='films.json';
-    $.getJSON(url, function (_data) {
-        data = _data;
-    })
-}
-
 window.onload = function () {
     console.log("Onload")
-
-    init();
+    const url = 'films.json';
 
     $.getJSON(url, function (data) {
         console.log(data[0]);
         // console.log(data[0].casts);
 
-        const box = document.getElementById("cards");
+        const cards = document.getElementById("cards");
 
-        for(var i = 0, len = data.length; i<len; i++){
+        for (let i = 0, len = data.length; i < len; i++) {
             const newCard = document.createElement('div');
             newCard.className = 'single-card';
             newCard.id = 'movie-card-' + i;
@@ -34,27 +24,101 @@ window.onload = function () {
             newImageDiv.innerHTML = `<img src=${data[i].poster}>`;
             newCard.append(newImageDiv);
 
+            //INFO
             const newInfo = document.createElement('div');
             newInfo.className = 'movie-info';
-            newInfo.innerHTML = `<div class="info-title"><h1>${data[i].title}</h1></div>`;
+
+            //INFO-TITLE
+            const title = ControlLength(data[i].title, 20);
+
+            const newTitle = document.createElement('div');
+            newTitle.className = 'info-title';
+            newTitle.innerHTML = `<h1>${ControlLength(title, 20)}</h1>`;
+
+            newInfo.append(newTitle);
+
+            /***************/
+            /*INFO-PUB_DATE*/
+            /***************/
+            let pubDate = 'PUB DATE: ';
+
+            for (let j = 0; j < data[i].pubdate.length; j++) {
+                if (j > 0) {
+                    pubDate += ', '
+                }
+                pubDate += data[i].pubdate[j];
+            }
+
+            const newPubDate = document.createElement('div');
+            newPubDate.className = 'info-pub_date';
+            newPubDate.innerHTML = `<h2>${ControlLength(pubDate, 60)}</h2>`
+
+            newInfo.append(newPubDate);
+
+            /**************/
+            /*INFO-COUNTRY*/
+            /**************/
+
+            let country = 'COUNTRY: ';
+
+            for(let j=0; j<data[i].countries.length; j++){
+                if (j>0){
+                    country+=', ';
+                }
+                country += data[i].countries[j];
+            }
+
+            const newCountry = document.createElement('div');
+            newCountry.className = 'info-country';
+            newCountry.innerHTML = `<h2>${country}</h2>`;
+
+            newInfo.append(newCountry);
+
+            /************/
+            /*INFO-CASTS*/
+            /************/
+            let casts = 'CASTS: ';
+
+            for (let j = 0; j < data[i].casts.length; j++) {
+                if (j > 0) {
+                    casts += ', ';
+                }
+                casts += data[i].casts[j].name;
+            }
+
+            const newCasts = document.createElement('div');
+            newCasts.className = 'info-cast';
+            newCasts.innerHTML = `<h3>${ControlLength(casts, 60)}</h3>`;
+
+            newInfo.append(newCasts);
+
+            // newInfo.innerHTML = `<div class="info-title"><h1>${title}</h1></div>`;
             // <div class="info-pub_date"><h2>${data[i].pubDate}</h2></div>
             // <div class="info-cast"><h3>${data[i].cast}</h3>;
 
             newCard.append(newInfo);
 
-            box.append(newCard);
+            cards.append(newCard);
         }
 
     })
 
 };
 
+/**
+ * @return {string}
+ */
+function ControlLength(str, maxLen) {
+    return str.length > maxLen ? str.slice(0, maxLen - 1) + '...' : str;
+}
 
-//
-// function createSingleMovie(index) {
-//     if (typeof index !== 'number') {
-//         throw 'Invalid index'
-//     }
-//
-// }
+function ExtractElement(str, originalArray) {
+    for (let i = 0; i < originalArray.length; i++) {
+        str += originalArray[i];
 
+        if (i > 0) {
+            str += ', '
+        }
+    }
+    return str;
+}
