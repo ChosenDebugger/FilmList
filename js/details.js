0d98943152276bd87aee2681d852ee1ec7c9246c
齐旭晨 <1023578360@qq.com>
2019-04-27 17:24:24 +0800

    done

---
+++ js/details.js
@@ -0,0 +1,186 @@
+'use strict';
+
+window.onload = function () {
+    console.log("Onload")
+
+    const id = window.location.search.slice(1);
+
+    init_with_id(id);
+};
+
+function init_with_id(id) {
+    $.getJSON('films.json',function (data) {
+        let targetMovie;
+        for (let i=0; i<data.length; i++){
+            if (data[i]._id === id){
+                targetMovie = data[i];
+            }
+        }
+        console.log(targetMovie);
+        if (targetMovie === undefined){
+            console.log('Invalid ID');
+            return;
+        }
+
+        //reset
+        const card = document.getElementById('detail-card');
+        while (card.children.length > 0) {
+            card.removeChild(card.children[0]);
+        }
+
+        //TITLE
+        const newTitle = document.createElement('div');
+        newTitle.className = 'detail-title';
+        newTitle.innerHTML = `<h1>${targetMovie.title}</h1>`;
+
+        card.append(newTitle);
+
+        //POSTER
+        const newPoster = document.createElement('div');
+        newPoster.className = 'detail-poster';
+        newPoster.innerHTML = `<img src=${targetMovie.poster}>`;
+
+        card.append(newPoster);
+
+        //INFO
+        const newInfo = document.createElement('div');
+        newInfo.className = 'detail-info';
+
+        //INFO-DIRECTOR
+        let directors = 'Directors: ';
+
+        for(let j = 0; j<targetMovie.directors.length; j++){
+            if (j>0){
+                directors += ' / ';
+            }
+            directors += targetMovie.directors[j].name;
+        }
+
+        const newDirector = document.createElement('div');
+        newDirector.className = 'info-director';
+        newDirector.innerHTML = `<h2>${directors}</h2>`;
+
+        newInfo.append(newDirector);
+
+        //INFO-WRITER
+        let writers = 'Writers: ';
+
+        for(let j = 0; j<targetMovie.writers.length; j++){
+            if (j>0){
+                writers+=' / ';
+            }
+            writers += targetMovie.writers[j].name;
+        }
+
+        const newWriter = document.createElement('div');
+        newWriter.className = 'info-writers';
+        newWriter.innerHTML = `<h2>${writers}</h2>`;
+
+        newInfo.append(newWriter);
+
+        //INFO-CAST
+        let casts = 'Casts: ';
+
+        for(let j = 0; j<targetMovie.casts.length; j++){
+            if (j>0){
+                casts+=' / ';
+            }
+            casts += targetMovie.casts[j].name;
+        }
+
+        const newCast = document.createElement('div');
+        newCast.className = 'info-casts';
+        newCast.innerHTML = `<h2>${casts}</h2>`;
+
+        newInfo.append(newCast);
+
+        //INFO-GENRES
+        let genres = 'Genres: ';
+
+        for(let j = 0; j<targetMovie.genres.length; j++){
+            if (j>0){
+                genres+=' / ';
+            }
+            genres += targetMovie.genres[j];
+        }
+
+        const newGenre = document.createElement('div');
+        newGenre.className = 'info-genres';
+        newGenre.innerHTML = `<h2>${genres}</h2>`;
+
+        newInfo.append(newGenre);
+
+        //INFO-LANGUAGE
+        let languages = 'Languages: ';
+
+        for(let j = 0; j<targetMovie.languages.length; j++){
+            if (j>0){
+                languages+=' / ';
+            }
+            languages += targetMovie.languages[j];
+        }
+
+        const newLan = document.createElement('div');
+        newLan.className = 'info-language';
+        newLan.innerHTML = `<h2>${languages}</h2>`;
+
+        newInfo.append(newLan);
+
+        //INFO-PUB_DATE
+        let pub_date = 'Pub-Date: ';
+
+        for(let j = 0; j<targetMovie.pubdate.length; j++){
+            if (j>0){
+                pub_date+=' / ';
+            }
+            pub_date += targetMovie.pubdate[j];
+        }
+
+        const newPub = document.createElement('div');
+        newPub.className = 'info-pub_date';
+        newPub.innerHTML = `<h2>${pub_date}</h2>`;
+
+        newInfo.append(newPub);
+
+        //INFO-DURATION
+        let duration = 'Duration: ' + targetMovie.duration;
+
+        const newDur = document.createElement('div');
+        newDur.className = 'info-duration';
+        newDur.innerHTML = `<h2>${duration}</h2>`;
+
+        newInfo.append(newDur);
+
+        //INFO-IMDB
+        let imdb = 'IMDB: ' + targetMovie.imdb;
+
+        const newIMDB = document.createElement('div');
+        newIMDB.className = 'info-imdb';
+        newDur.innerHTML = `<h2>${imdb}</h2>`;
+
+        newInfo.append(newIMDB);
+
+        card.append(newInfo);
+
+        //SUMMARY
+        const newSummary = document.createElement('div');
+        newSummary.className = 'detail-summary';
+
+        const newHeader = document.createElement('div');
+        newHeader.className = 'header-in-summary';
+        newHeader.innerHTML = `<h1>${targetMovie.title}的剧情简介</h1>`;
+
+        newSummary.append(newHeader);
+
+        const newContent = document.createElement('div');
+        newContent.className = 'content';
+        newContent.innerHTML = `<p>&nbsp&nbsp&nbsp&nbsp${targetMovie.summary}</p>`;
+
+        newSummary.append(newContent);
+
+        card.append(newSummary);
+
+    })
+
+}
+
---
+++ js/homepage.js
@@ -0,0 +1,199 @@
+'use strict';
+
+window.onload = function () {
+    console.log("Onload")
+
+    init_with_pageNum(0);
+};
+
+
+function PageTurning(pageNum) {
+    $.getJSON('films.json', function (data) {
+        let currentPageNum = -1;
+        const currentFirstCard = document.getElementById('movie-card-0').firstChild.firstChild;
+        for (let i = 0; i < 200; i = i + 10) {
+            if (currentFirstCard.src === data[i].poster) {
+                currentPageNum = i / 10;
+            }
+        }
+
+        if (pageNum >= 0) {
+            init_with_pageNum(pageNum);
+            if (currentPageNum !== pageNum) {
+                window.scroll(0, 0);
+            }
+        } else if (pageNum === -1) {
+            if (currentPageNum === 0) {
+                init_with_pageNum(0);
+            } else {
+                init_with_pageNum(currentPageNum - 1);
+                window.scroll(0, 0);
+            }
+        } else if (pageNum === -2) {
+            if (currentPageNum === 9) {
+                init_with_pageNum(9);
+            } else {
+                init_with_pageNum(currentPageNum + 1);
+                window.scroll(0, 0);
+            }
+        } else {
+            console.log('Invalid Page');
+        }
+    })
+}
+
+function ShowDetails(movieID) {
+    window.location.href = "details.html?" + movieID;
+}
+
+
+function init_with_pageNum(pageNum) {
+    const url = 'films.json';
+
+    $.getJSON(url, function (data) {
+        // console.log(data);
+        // console.log(data[0].casts);
+
+        const cards = document.getElementById("cards");
+        while (cards.children.length > 0) {
+            cards.removeChild(cards.children[0]);
+        }
+
+        for (let i = pageNum * 10; i < pageNum * 10 + 10; i++) {
+            const newCard = document.createElement('div');
+            newCard.className = 'single-card';
+            newCard.id = 'movie-card-' + i % 10;
+
+            const newImageDiv = document.createElement('div');
+            newImageDiv.className = 'movie-image';
+            newImageDiv.innerHTML = `<img src=${data[i].poster}>`;
+            newCard.append(newImageDiv);
+
+            //INFO
+            const newInfo = document.createElement('div');
+            newInfo.className = 'movie-info';
+
+            /************/
+            /*INFO-TITLE*/
+            /************/
+            const id = data[i]._id;
+            const title = data[i].title;
+            const newTitle = document.createElement('div');
+            newTitle.className = 'info-title';
+            newTitle.innerHTML = `<a href="javascript:ShowDetails(${id})">${title}</a>`;
+            newInfo.append(newTitle);
+
+            /***************/
+            /*INFO-PUB_DATE*/
+            /***************/
+            let pubDate = 'PUB DATE: ';
+
+            for (let j = 0; j < data[i].pubdate.length; j++) {
+                if (j > 0) {
+                    pubDate += ', '
+                }
+                pubDate += data[i].pubdate[j];
+            }
+
+            const newPubDate = document.createElement('div');
+            newPubDate.className = 'info-pub_date';
+            newPubDate.innerHTML = `<h2>${pubDate}</h2>`;
+
+            newInfo.append(newPubDate);
+
+            /**************/
+            /*INFO-COUNTRY*/
+            /**************/
+
+            let country = 'COUNTRY: ';
+
+            for (let j = 0; j < data[i].countries.length; j++) {
+                if (j > 0) {
+                    country += ', ';
+                }
+                country += data[i].countries[j];
+            }
+
+            const newCountry = document.createElement('div');
+            newCountry.className = 'info-country';
+            newCountry.innerHTML = `<h2>${country}</h2>`;
+
+            newInfo.append(newCountry);
+
+            /************/
+            /*INFO-CASTS*/
+            /************/
+            let casts = 'CASTS: ';
+
+            for (let j = 0; j < data[i].casts.length; j++) {
+                if (j > 0) {
+                    casts += ', ';
+                }
+                casts += data[i].casts[j].name;
+            }
+
+            const newCasts = document.createElement('div');
+            newCasts.className = 'info-cast';
+            newCasts.innerHTML = `<h2>${casts}</h2>`;
+
+            newInfo.append(newCasts);
+
+            /*************/
+            /*INFO-GENRES*/
+            /*************/
+            let genres = 'GENRES:';
+
+            for (let j = 0; j < data[i].genres.length; j++) {
+                if (j > 0) {
+                    genres += ' ';
+                }
+                genres += data[i].genres[j];
+            }
+
+            const newGenres = document.createElement('div');
+            newGenres.className = 'info-genres';
+            newGenres.innerHTML = `<h2>${genres}</h2>`;
+
+            newInfo.append(newGenres);
+
+            /***************/
+            /*INFO-DURATION*/
+            /***************/
+
+            let duration = 'DURATION: ' + data[i].duration;
+
+            const newDuration = document.createElement('div');
+            newDuration.className = 'info-duration';
+            newDuration.innerHTML = `<h2>${duration}</h2>`;
+
+            newInfo.append(newDuration);
+
+            newCard.append(newInfo);
+
+            cards.append(newCard);
+        }
+    });
+
+    const footer = document.getElementById('pagination-ul');
+    while(footer.children.length>0){
+        footer.removeChild(footer.children[0]);
+    }
+
+    const pageUp = document.createElement('li');
+    pageUp.innerHTML = `<a href="javascript:PageTurning(-1)"></a>`;
+    footer.append(pageUp);
+
+    for(let i=0;i<10;i++){
+        const pageFlip = document.createElement('li');
+        pageFlip.innerHTML = `<a href="javascript:PageTurning(${i})"></a>`;
+        footer.append(pageFlip);
+    }
+
+    const pageDown = document.createElement('li');
+    pageDown.innerHTML = `<a href="javascript:PageTurning(-2)"></a>`;
+    footer.append(pageDown);
+}
+
+function Reload() {
+    location.reload();
+}
